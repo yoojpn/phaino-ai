b0a15a710e51e57aa699a3065291e838d6cbdcb1
yoojpn <lcdcreate.info@gmail.com>
2026-05-05 03:36:22 +0000

    add _worker.js for api proxy

---
+++ cf-pages/_worker.js
@@ -0,0 +1,14 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/api/')) {
+      const targetUrl = 'http://152.69.200.74:8080' + url.pathname + url.search;
+      return fetch(targetUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
+      });
+    }
+    return env.ASSETS.fetch(request);
+  }
+}
