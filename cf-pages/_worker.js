091c7bad5aa1a40b8ca8eb5e8bd5f31640c4dfb0
yoojpn <lcdcreate.info@gmail.com>
2026-05-05 03:43:25 +0000

    fix: remove auth headers for api proxy

--- cf-pages/_worker.js
+++ cf-pages/_worker.js
@@ -3,9 +3,12 @@ export default {
     const url = new URL(request.url);
     if (url.pathname.startsWith('/api/')) {
       const targetUrl = 'http://152.69.200.74:8080' + url.pathname + url.search;
+      const newHeaders = new Headers(request.headers);
+      newHeaders.delete('Cookie');
+      newHeaders.delete('Authorization');
       return fetch(targetUrl, {
         method: request.method,
-        headers: request.headers,
+        headers: newHeaders,
         body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
       });
     }
